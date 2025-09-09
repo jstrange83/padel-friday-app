794f427cb30fc3103c38f089483d707606083ad3
jstrange83 <jstrange83@gmail.com>
2025-09-09 21:43:25 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,1 +1,6 @@
-src/main.tsx
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App'
+
+const el = document.getElementById('root')!
+createRoot(el).render(<App />)
