1ba705a524467fee047ad44236d42e72536a9882
jstrange83 <jstrange83@gmail.com>
2025-09-11 14:08:45 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
+// src/main.tsx
 import React from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css"; // VIGTIGT: styles ind
+import "./index.css"; // <- denne linje sikrer at styling loader
 
-createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
